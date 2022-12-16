f7435f52827871bc041ab93c44ea9214cb219319
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2022-12-16 20:51:35 +0600

    Delete install_firefox.sh

--- script/install_firefox.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ex
-wget -O ~/FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64"
-sudo tar xjf ~/FirefoxSetup.tar.bz2 -C /opt/
-sudo ln -s /opt/firefox/firefox /usr/lib/firefox/firefox
